a33c6e9eebc5011e253724ad19e96dbf163d3ac4
Lucía Rivas <lucia_rm98@hotmail.com>
2019-03-15 19:00:17 +0100

    imprime por pantalla

--- pra2/det.asm
+++ pra2/det.asm
@@ -10,7 +10,8 @@
 DATOS SEGMENT 
 
 A	DB	1, 2, 3, -2, 3, 1, -1, -1, -1
-
+Texto DB "Seleccione una opcion:",13,10, "1) Calcular el determinante con valores por defecto",13,10, "2) Calcular el determinante con valores introducidos por teclado", 13, 10, '$' 
+OPCION DB 3 DUP(?) 		; Guarda la opcion escrita por teclado en OPCION[2]
 DATOS ENDS 
 
 
@@ -53,7 +54,28 @@ START PROC NEAR
 
     ; FIN DE LAS INICIALIZACIONES 
 	
-	CALL POS
+	
+	MOV DX, OFFSET Texto 		; DX = offset al inicio del texto a imprimir
+	MOV AH, 9 					; Número de función = 9 para imprimir un string
+	INT 21h 					; Imprime por pantalla que opcion debe escoger 
+	
+	MOV AH,0AH 					; Capturamos por teclado la opcion a ejecutar(1 o 2)
+	MOV DX, OFFSET OPCION 		; Reservamos OPCION para la opcion introducida por teclado
+	MOV OPCION[0],3 			; Lectura de caracteres máxima=60
+	INT 21H 
+	
+	CMP OPCION[2], 1h			; Vemos que opcion hemos elegido
+	JE OP1 						; Si es 1 saltamos a OP1 y calculamos el determinante por defecto
+	JMP OP2						; Si no es 1 (es 2) saltamos a OP2 y le pide la matriz por teclado
+	
+OP2:
+
+	
+OP1:	
+	CALL POS                    ; Llamamos a la función que calcula la parte positiva del determinante
+	MOV DX, CX					; Copiamos el resultado de POS a DX 
+	CALL NEGAT 					; Llamamos a la función que calcula la parte negativa del determinante 
+	SUB DX, CX					; Restamos al resultado positivo el resultado negativo
 
     ; FIN DEL PROGRAMA 
     MOV AX, 4C00H 
@@ -70,11 +92,58 @@ POS PROC NEAR
     MOV AL, A[0h]				; Empezamos por la diagonal, las posiciones 0h,4h y 8h de la matriz
     IMUL A[4h]					; Multiplicamos A[0h] por A[4h], y se almacena el resultado en AX
 	IMUL A[8h]			; Multiplicamos A[8h], casteandolo a word, por el resultado almacenado en AX. 
-	MOV CX, AX							; El resultado queda almacenado en AX, con DX = 0 ya que los números son de 5 bits. Lo movemos a CX
+	MOV CX, AX					; El resultado queda almacenado en AX, con DX = 0 ya que los números son de 5 bits. Lo movemos a CX
+	
+	MOV AL, A[2h]				; Continuamos por los elementos 2h, 3h y 7h
+    IMUL A[3h]					; Multiplicamos A[2h] por A[3h], y se almacena el resultado en AX
+	IMUL A[7h]			; Multiplicamos A[7h], casteandolo a word, por el resultado almacenado en AX.
+	ADD CX, AX					; Sumamos el resultado a CX, donde guardamos el resultado anterior
+	
+	MOV AL, A[1h]				; Finalizamos con los elementos 1h, 5h y 6h
+    IMUL A[5h]					; Multiplicamos A[1h] por A[5h], y se almacena el resultado en AX
+	IMUL A[6h]			; Multiplicamos A[6h], casteandolo a word, por el resultado almacenado en AX.
+	ADD CX, AX					; Finalmente sumamos de nuevo a CX el resultado final
 	RET
 	
 	
 POS ENDP 
+
+;______________________________________________________________________
+; SUBRUTINA PARA CALCULAR LA PARTE NEGATIVA DEL DETERMINANTE
+; ENTRADA 
+; SALIDA CX=RESULTADO
+;______________________________________________________________________
+NEGAT PROC NEAR 
+	
+    MOV AL, A[2h]				; Empezamos por la diagonal, las posiciones 2h,4h y 6h de la matriz
+    IMUL A[4h]					; Multiplicamos A[2h] por A[4h], y se almacena el resultado en AX
+	IMUL A[6h]					; Multiplicamos A[6h], casteandolo a word, por el resultado almacenado en AX. 
+	MOV CX, AX					; El resultado queda almacenado en AX, con DX = 0 ya que los números son de 5 bits. Lo movemos a CX
+	
+	MOV AL, A[1h]				; Continuamos por los elementos 1h, 3h y 8h
+    IMUL A[3h]					; Multiplicamos A[1h] por A[3h], y se almacena el resultado en AX
+	IMUL A[8h]					; Multiplicamos A[8h], casteandolo a word, por el resultado almacenado en AX.
+	ADD CX, AX					; Sumamos el resultado a CX, donde guardamos el resultado anterior
+	
+	MOV AL, A[0h]				; Finalizamos con los elementos 0h, 5h y 7h
+    IMUL A[5h]					; Multiplicamos A[0h] por A[5h], y se almacena el resultado en AX
+	IMUL A[7h]					; Multiplicamos A[7h], casteandolo a word, por el resultado almacenado en AX.
+	ADD CX, AX					; Finalmente sumamos de nuevo a CX el resultado final
+	RET
+	
+	
+NEGAT ENDP 
+
+;______________________________________________________________________
+; SUBRUTINA PARA CONVERTIR UN NÚMERO A ASCII
+; ENTRADA 
+; SALIDA 
+;______________________________________________________________________
+ASCII PROC NEAR
+	
+	
+
+ASCII ENDP
 
 ; FIN DEL SEGMENTO DE CODIGO 
 CODE ENDS 
